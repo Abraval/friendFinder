eb4ff41aefee51b5ccda80a7421e9edbf054777c
Valentine <abraval@gmail.com>
2019-11-21 01:47:26 -0500

    adde js in index.html to capture customer's input and add it to api/friends

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,4 +4,12 @@ module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friendsArray)
     });
+
+    app.post("/api/friends", function(req, res) {
+    
+        friendsArray.push(req.body)
+        res.json(true)
+        return res.end();
+      });
 }
+
