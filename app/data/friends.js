2f73aec610f5d8945176a9d693be6033ff8f2e5a
Valentine <abraval@gmail.com>
2019-11-20 22:57:04 -0500

    added routs and updated server info

---
+++ app/data/friends.js
@@ -0,0 +1,58 @@
+var friendsArray = [
+  {
+    name: "Jennifer Garner",
+    photo:
+      "https://www.hellomagazine.com/imagenes/celebrities/2019093078444/jennifer-garner-unrecognisable-bowl-haircut-photo/0-379-397/jennifer-garner-unrecognisable-hair-t.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Jennifer Aniston",
+    photo:
+      "https://guinnessworldrecords.com/Images/jennifer-aniston_tcm25-595487.jpg",
+    scores: [1, 2, 5, 3, 1, 2, 3, 1, 2, 4]
+  },
+  {
+    name: "Beyoncé",
+    photo:
+      "https://static-showpoblog.executiveponies.com/uploads/sites/1/2019/09/BEYONCE-e1567559551763-780x405.jpg",
+    scores: [3, 2, 5, 4, 2, 5, 5, 1, 4, 5]
+  },
+  {
+    name: "Ryan Gosling",
+    photo:
+      "https://ksassets.timeincuk.net/wp/uploads/sites/55/2016/11/RyanGoslingGettyImages-531518458-620x394.jpg",
+    scores: [1, 5, 2, 2, 1, 5, 2, 1, 2, 5]
+  },
+  {
+    name: "Idris Elba",
+    photo:
+      "https://cdn.thegentlemansjournal.com/wp-content/uploads/2016/09/Idris-elba-2-1-900x600-c-center.jpg",
+    scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+  },
+  {
+    name: "David Beckham",
+    photo:
+      "https://images.businessoffashion.com/profiles/asset/1715/a1f88b4a3c5c5b6761438553fe38ab872d9f59cc.jpeg?auto=format%2Ccompress&fit=crop&h=360&w=660",
+    scores: [5, 4, 3, 2, 1, 5, 4, 3, 2, 1]
+  },
+  {
+    name: "Barack Obama",
+    photo:
+      "https://www.indiewire.com/wp-content/uploads/2018/12/Screen-Shot-2018-12-28-at-11.06.14-AM.png?w=780",
+    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+  },
+  {
+    name: "Elvis Presley",
+    photo:
+      "https://katv.com/resources/media/4fbe5c8e-f78f-4ad5-a618-0786331b5c44-large16x9_1280x720_70816P00NYOGZ.png?1519733699308",
+    scores: [4, 5, 3, 4, 2, 3, 1, 2, 4, 5]
+  },
+  {
+    name: "Marilyn Monroe",
+    photo:
+      "https://www.biography.com/.image/c_fill%2Ccs_srgb%2Cfl_progressive%2Ch_400%2Cq_auto:good%2Cw_620/MTY2NzA3MDU5MTEzMzM4MTQ4/marilyn_monroe_photo_alfred_eisenstaedt_pix_inc_the_life_picture_collection_getty_images_53376357_cropped.jpg",
+    scores: [3, 1, 4, 2, 5, 3, 4, 2, 3, 1]
+  }
+];
+
+module.exports = friendsArray;
--- server.js
+++ server.js
@@ -1,24 +1,20 @@
-var bodyParser = require('body-parser')
-const express = require('express')
+var bodyParser = require('body-parser');
+const express = require('express');
+var path = require('path');
 
-const app = express()
+
+const app = express();
 var PORT = process.env.PORT || 3000;
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(__dirname + '/app/public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
  
 // parse application/json
-app.use(bodyParser.json())
- 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-})
+app.use(bodyParser.json());
 
+require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
 
 app.listen(PORT, function(){
 
